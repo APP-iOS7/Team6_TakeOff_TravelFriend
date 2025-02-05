06119123f448105c1cd6be4c1275438cd2e0f84b
hiereit-dev <https://github.com/hiereit-dev>
2025-02-05 13:18:40 +0900

    feat: 여행추가화면/기능 구현

---
+++ TravelFriend/TravelFriend/Views/AddTravelView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTravelView.swift
+//  TravelFriend
+//
+//  Created by 박세라 on 2/5/25.
+//
+
+import SwiftUI
+
+struct AddTravelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTravelView()
+}
