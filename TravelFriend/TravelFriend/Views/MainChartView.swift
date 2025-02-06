4ca6331c7f65265b1eb1cc74c1ae48b27a80c9c0
AIsiteru98 <dlwodyd0517@gmail.com>
2025-02-06 17:54:27 +0900

    commit: 진짜찐막 익스펜스없을때 대체이미지 삽입

--- TravelFriend/TravelFriend/Views/MainChartView.swift
+++ TravelFriend/TravelFriend/Views/MainChartView.swift
@@ -24,62 +24,50 @@ struct MainChartView: View {
         }
     }
     var body: some View {
-        Chart {
-            ForEach(dailyExpenses) { expense in
-                BarMark(
-                    x: .value("여행 일자", "\(expense.day)일차 경비"), // 문자열 유지
-                    y: .value("총 비용합", expense.price),
-                    width: .fixed(30) // 막대 너비 고정
-                )
-                .foregroundStyle(by: .value("Shape Color", expense.category))
-            }
-        }
-        .chartXVisibleDomain(length: 5)
-        .chartScrollableAxes(.horizontal)
-        .frame(height: 300)
-        .onAppear(perform: self.fetchDailyExpenses)
         
+        if dailyExpenses.isEmpty {
+            MainChartEmptyView()
+        } else {
+            Chart {
+                ForEach(dailyExpenses) { expense in
+                    BarMark(
+                        x: .value("여행 일자", "\(expense.day)일차 경비"), // 문자열 유지
+                        y: .value("총 비용합", expense.price),
+                        width: .fixed(30) // 막대 너비 고정
+                    )
+                    .foregroundStyle(by: .value("Shape Color", expense.category))
+                }
+            }
+            .chartXVisibleDomain(length: 5)
+            .chartScrollableAxes(.horizontal)
+            .frame(height: 300)
+            .onAppear(perform: self.fetchDailyExpenses)
+        }
     }
 }
 extension MainChartView {
     func getCntOfDays() -> Int {
-            return Set(dailyExpenses.map { $0.day }).count
+        return Set(dailyExpenses.map { $0.day }).count
+    }
+}
+
+struct MainChartEmptyView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "chart.bar.xaxis.ascending")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50)
+            
+            Text("차트를 표시하기 위해\n지출 내역을 입력해 주세요")
+                .padding()
+                .multilineTextAlignment(.center)
         }
+        
+            .frame(maxWidth: .infinity, maxHeight: 300)
+    }
 }
 
 #Preview {
-    MainChartView(dailyExpenses: [
-        DailyExpense(day: 1, category: "Food", price: 12.5),
-        DailyExpense(day: 1, category: "Transport", price: 8.0),
-        DailyExpense(day: 1, category: "Entertainment", price: 15.0),
-        DailyExpense(day: 2, category: "Groceries", price: 30.0),
-        DailyExpense(day: 2, category: "Utilities", price: 50.0),
-        DailyExpense(day: 3, category: "Shopping", price: 25.0),
-        DailyExpense(day: 4, category: "Food", price: 18.0),
-        DailyExpense(day: 3, category: "Transport", price: 7.5),
-        DailyExpense(day: 3, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 4, category: "Entertainment", price: 20.0),
-        DailyExpense(day: 5, category: "Food", price: 12.5),
-        DailyExpense(day: 6, category: "Transport", price: 8.0),
-        DailyExpense(day: 7, category: "Entertainment", price: 15.0),
-        DailyExpense(day: 8, category: "Groceries", price: 30.0),
-        DailyExpense(day: 9, category: "Utilities", price: 50.0),
-        DailyExpense(day: 10, category: "Shopping", price: 25.0),
-        DailyExpense(day: 11, category: "Food", price: 18.0),
-        DailyExpense(day: 12, category: "Transport", price: 7.5),
-        DailyExpense(day: 13, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 14, category: "Entertainment", price: 20.0),
-        DailyExpense(day: 15, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 16, category: "Entertainment", price: 20.0),
-        DailyExpense(day: 17, category: "Food", price: 12.5),
-        DailyExpense(day: 18, category: "Transport", price: 8.0),
-        DailyExpense(day: 19, category: "Entertainment", price: 15.0),
-        DailyExpense(day: 20, category: "Groceries", price: 30.0),
-        DailyExpense(day: 21, category: "Utilities", price: 50.0),
-        DailyExpense(day: 23, category: "Shopping", price: 25.0),
-        DailyExpense(day: 22, category: "Food", price: 18.0),
-        DailyExpense(day: 23, category: "Transport", price: 7.5),
-        DailyExpense(day: 13, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 14, category: "Entertainment", price: 20.0)
-    ])
+    MainChartView(dailyExpenses: [])
 }
