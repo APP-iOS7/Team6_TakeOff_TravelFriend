db6edce45eb8f1ed56194b040d708afdd18bfabc
AIsiteru98 <dlwodyd0517@gmail.com>
2025-02-06 13:37:30 +0900

    update: 차트 막대그래프 두꺠 고정, 스크롤 가능하게 수정

--- TravelFriend/TravelFriend/Views/MainChartView.swift
+++ TravelFriend/TravelFriend/Views/MainChartView.swift
@@ -21,33 +21,18 @@ struct MainChartView: View {
             dailyExpenses = expenses
         }
     }
-    
-    
-    let sampleExpenses: [DailyExpense] = [
-        DailyExpense(day: 1, category: "Food", price: 12.5),
-        DailyExpense(day: 1, category: "Transport", price: 8.0),
-        DailyExpense(day: 1, category: "Entertainment", price: 15.0),
-        DailyExpense(day: 2, category: "Groceries", price: 30.0),
-        DailyExpense(day: 2, category: "Utilities", price: 50.0),
-        DailyExpense(day: 2, category: "Shopping", price: 25.0),
-        DailyExpense(day: 3, category: "Food", price: 18.0),
-        DailyExpense(day: 3, category: "Transport", price: 7.5),
-        DailyExpense(day: 3, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 3, category: "Entertainment", price: 20.0),
-        DailyExpense(day: 4, category: "Transport", price: 7.5),
-        DailyExpense(day: 4, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 4, category: "Entertainment", price: 20.0)
-    ]
     var body: some View {
         Chart {
             ForEach(dailyExpenses) { expense in
                 BarMark(
-                    x: .value("여행 일자", "\(expense.day)일차 여행"), // 어느 위치에 막대그래프를 쌓을 건가
-                    y: .value("총 비용합", expense.price) // 값은 어떻게 매길 건가
+                    x: .value("여행 일자", "\(expense.day)일차 경비"), // 어느 위치에 막대그래프를 쌓을 건가
+                    y: .value("총 비용합", expense.price), // 값은 어떻게 매길 건가
+                    width: .fixed(30)
                 )
                 .foregroundStyle(by: .value("Shape Color", expense.category)) // <- 색상 값
             }
         }
+        .chartScrollableAxes(.horizontal) // enable scroll
         .frame(height: 300)
         .onAppear(perform: self.fetchDailyExpenses)
         
@@ -55,5 +40,16 @@ struct MainChartView: View {
 }
 
 //#Preview {
-//    MainChartView()
+//    MainChartView(dailyExpenses: [
+//        DailyExpense(day: 1, category: "Food", price: 12.5),
+//        DailyExpense(day: 2, category: "Transport", price: 8.0),
+//        DailyExpense(day: 3, category: "Entertainment", price: 15.0),
+//        DailyExpense(day: 4, category: "Groceries", price: 30.0),
+//        DailyExpense(day: 5, category: "Utilities", price: 50.0),
+//        DailyExpense(day: 6, category: "Shopping", price: 25.0),
+//        DailyExpense(day: 7, category: "Food", price: 18.0),
+//        DailyExpense(day: 8, category: "Transport", price: 7.5),
+//        DailyExpense(day: 9, category: "Healthcare", price: 40.0),
+//        DailyExpense(day: 10, category: "Entertainment", price: 20.0)
+//    ])
 //}
