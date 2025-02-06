1d5ed7b946019db3ba759c64d22e3efaee89e69d
AIsiteru98 <dlwodyd0517@gmail.com>
2025-02-06 17:37:10 +0900

    항상 날짜 순서대로 나오도록 고정

--- TravelFriend/TravelFriend/Views/MainChartView.swift
+++ TravelFriend/TravelFriend/Views/MainChartView.swift
@@ -18,7 +18,9 @@ struct MainChartView: View {
         dbManager = DBManager(modelContext: modelContext)
         
         if let expenses = dbManager?.fetchExpenses() {
-            dailyExpenses = expenses
+            dailyExpenses = expenses.sorted(by: { one, two in
+                one.day < two.day
+            })
         }
     }
     var body: some View {
@@ -32,8 +34,8 @@ struct MainChartView: View {
                 .foregroundStyle(by: .value("Shape Color", expense.category))
             }
         }
-        .chartXVisibleDomain(length: 1)
-        .chartScrollableAxes(getCntOfDays() >= 10 ? .horizontal : [])
+        .chartXVisibleDomain(length: 5)
+        .chartScrollableAxes(.horizontal)
         .frame(height: 300)
         .onAppear(perform: self.fetchDailyExpenses)
         
@@ -56,28 +58,28 @@ extension MainChartView {
         DailyExpense(day: 4, category: "Food", price: 18.0),
         DailyExpense(day: 3, category: "Transport", price: 7.5),
         DailyExpense(day: 3, category: "Healthcare", price: 40.0),
-        DailyExpense(day: 4, category: "Entertainment", price: 20.0)
-//        DailyExpense(day: 5, category: "Food", price: 12.5),
-//        DailyExpense(day: 6, category: "Transport", price: 8.0),
-//        DailyExpense(day: 7, category: "Entertainment", price: 15.0),
-//        DailyExpense(day: 8, category: "Groceries", price: 30.0),
-//        DailyExpense(day: 9, category: "Utilities", price: 50.0),
-//        DailyExpense(day: 10, category: "Shopping", price: 25.0),
-//        DailyExpense(day: 11, category: "Food", price: 18.0),
-//        DailyExpense(day: 12, category: "Transport", price: 7.5),
-//        DailyExpense(day: 13, category: "Healthcare", price: 40.0),
-//        DailyExpense(day: 14, category: "Entertainment", price: 20.0),
-//        DailyExpense(day: 15, category: "Healthcare", price: 40.0),
-//        DailyExpense(day: 16, category: "Entertainment", price: 20.0),
-//        DailyExpense(day: 17, category: "Food", price: 12.5),
-//        DailyExpense(day: 18, category: "Transport", price: 8.0),
-//        DailyExpense(day: 19, category: "Entertainment", price: 15.0),
-//        DailyExpense(day: 20, category: "Groceries", price: 30.0),
-//        DailyExpense(day: 21, category: "Utilities", price: 50.0),
-//        DailyExpense(day: 23, category: "Shopping", price: 25.0),
-//        DailyExpense(day: 22, category: "Food", price: 18.0),
-//        DailyExpense(day: 23, category: "Transport", price: 7.5),
-//        DailyExpense(day: 13, category: "Healthcare", price: 40.0),
-//        DailyExpense(day: 14, category: "Entertainment", price: 20.0)
+        DailyExpense(day: 4, category: "Entertainment", price: 20.0),
+        DailyExpense(day: 5, category: "Food", price: 12.5),
+        DailyExpense(day: 6, category: "Transport", price: 8.0),
+        DailyExpense(day: 7, category: "Entertainment", price: 15.0),
+        DailyExpense(day: 8, category: "Groceries", price: 30.0),
+        DailyExpense(day: 9, category: "Utilities", price: 50.0),
+        DailyExpense(day: 10, category: "Shopping", price: 25.0),
+        DailyExpense(day: 11, category: "Food", price: 18.0),
+        DailyExpense(day: 12, category: "Transport", price: 7.5),
+        DailyExpense(day: 13, category: "Healthcare", price: 40.0),
+        DailyExpense(day: 14, category: "Entertainment", price: 20.0),
+        DailyExpense(day: 15, category: "Healthcare", price: 40.0),
+        DailyExpense(day: 16, category: "Entertainment", price: 20.0),
+        DailyExpense(day: 17, category: "Food", price: 12.5),
+        DailyExpense(day: 18, category: "Transport", price: 8.0),
+        DailyExpense(day: 19, category: "Entertainment", price: 15.0),
+        DailyExpense(day: 20, category: "Groceries", price: 30.0),
+        DailyExpense(day: 21, category: "Utilities", price: 50.0),
+        DailyExpense(day: 23, category: "Shopping", price: 25.0),
+        DailyExpense(day: 22, category: "Food", price: 18.0),
+        DailyExpense(day: 23, category: "Transport", price: 7.5),
+        DailyExpense(day: 13, category: "Healthcare", price: 40.0),
+        DailyExpense(day: 14, category: "Entertainment", price: 20.0)
     ])
 }
